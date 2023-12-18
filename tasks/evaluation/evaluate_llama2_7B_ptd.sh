8c6a6abf97c246abeb64c6479b71a686befbd73e
wenjiang2357 <wenjiang@huawei.com>
2023-12-18 15:47:58 +0800

    llama2-7b 评估脚本

---
+++ tasks/evaluation/evaluate_llama2_7B_ptd.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib:/root/miniconda3/lib:$LD_LIBRARY_PATH
+export HCCL_CONNECT_TIMEOUT=1200
+export COMBINED_ENABLE=1
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh 
+
+# modify script model path and tokenizer path
+TOKENIZER_PATH=./llama2-7b-hf/  #tokenizer path
+CHECKPOINT=./llama2-7b-tp8pp1  #model path
+# configure task and data path
+DATA_PATH="./mmlu/data/test/"
+TASK="mmlu"
+
+# distributed config
+MASTER_ADDR=localhost
+MASTER_PORT=6011
+NNODES=1
+NODE_RANK=0
+NPUS_PER_NODE=8
+DISTRIBUTED_ARGS="--nproc_per_node $NPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+# configure generation parameters 
+python -m torch.distributed.launch $DISTRIBUTED_ARGS tasks/evaluation/evaluation_llama.py   \
+     --task-data-path $DATA_PATH \
+     --task $TASK \
+     --seq-length 4096 \
+     --max-new-tokens 1 \
+     --max-position-embeddings 4096 \
+     --tensor-model-parallel-size 8 \
+     --pipeline-model-parallel-size 1  \
+     --num-layers 32  \
+     --hidden-size 4096  \
+     --ffn-hidden-size 11008 \
+     --num-attention-heads 32  \
+     --mlp-layer-fusion \
+     --load ${CHECKPOINT}  \
+     --position-embedding-type rope \
+     --normalization RMSNorm \
+     --tokenizer-type PretrainedFromHF  \
+     --tokenizer-name-or-path ${TOKENIZER_PATH} \
+     --tokenizer-not-use-fast \
+     --fp16  \
+     --micro-batch-size 1  \
+     --seed 42 | tee logs/eval_mmlu.log
+
+
+
