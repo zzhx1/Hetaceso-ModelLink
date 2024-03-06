995eb1ca1821f156e60449b625240150e2f6910b
huangyiming <hym08280020@163.com>
2024-03-06 01:09:27 +0000

    !874 修改支持不均匀切分patch中的包导入bug Merge pull request !874 from huangyiming/modellink

--- modellink/adaptor_model.py
+++ modellink/adaptor_model.py
@@ -4,8 +4,10 @@ import megatron
 from megatron.model import GPTModel
 from megatron.model import transformer
 from megatron import get_args
+from megatron.core import mpu
 from megatron.core import tensor_parallel
 from megatron.core import parallel_state
+from megatron.core.enums import ModelType
 from megatron.model.module import MegatronModule
 from megatron.model.enums import AttnMaskType
 from megatron.model.language_model import parallel_lm_logits
