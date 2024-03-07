22c184321bd4c33b9315cdfa3e11159a4541ff86
guoxinjie <guoxinjie1@huawei.com>
2024-03-07 02:21:32 +0000

    !890 修改流水线配置路径参数，配置拉起命令路径 Merge pull request !890 from guoxinjie/path_fix

--- tests/pipeline/bloom-7B/bloom-7B.sh
+++ tests/pipeline/bloom-7B/bloom-7B.sh
@@ -1,6 +1,6 @@
 # Provide uniform access for piepline.
-python /tests/pipeline/bloom-7B/test_process_pretrain_data.py
-python /tests/pipeline/bloom-7B/test_convert_ckpt_from_huggingface.py
-pytest -s /tests/pipeline/bloom-7B/test_trainer.py
-pytest -s /tests/pipeline/bloom-7B/test_generation.py
-pytest -s /tests/pipeline/bloom-7B/test_evaluation.py
+python tests/pipeline/bloom-7B/test_process_pretrain_data.py
+python tests/pipeline/bloom-7B/test_convert_ckpt_from_huggingface.py
+pytest -s tests/pipeline/bloom-7B/test_trainer.py
+pytest -s tests/pipeline/bloom-7B/test_generation.py
+pytest -s tests/pipeline/bloom-7B/test_evaluation.py
