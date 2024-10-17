3afe8cde43d3f586855c3798543fe8705e57380d
曲玥泽 <quyueze@h-partners.com>
2024-10-17 03:28:34 +0000

    !1768 codellama-34b、llama-7b、yi-34b权重转换适配新框架 Merge pull request !1768 from 曲玥泽/master

--- examples/llama/ckpt_convert_llama_hf2legacy.sh
+++ examples/llama/ckpt_convert_llama_hf2legacy.sh
@@ -4,10 +4,11 @@ source /usr/local/Ascend/ascend-toolkit/set_env.sh
 # 权重格式转换
 python convert_ckpt.py \
     --model-type GPT \
-    --loader llama2_hf \
-    --saver megatron \
+    --load-model-type hf \
+    --save-model-type mg \
     --target-tensor-parallel-size 1 \
     --target-pipeline-parallel-size 8 \
     --load-dir ./model_from_hf/Llama-hf/ \
     --save-dir ./model_weights/Llama-legacy/ \
-    --tokenizer-model ./model_from_hf/Llama-hf/tokenizer.model
+    --tokenizer-model ./model_from_hf/Llama-hf/tokenizer.model \
+    --model-type-hf llama2
--- examples/yi/convert_ckpt_yi_hf2legacy.sh
+++ examples/yi/convert_ckpt_yi_hf2legacy.sh
@@ -3,11 +3,12 @@ source /usr/local/Ascend/ascend-toolkit/set_env.sh
 
 python convert_ckpt.py \
    --model-type GPT \
-   --loader llama2_hf \
-   --saver megatron \
+   --load-model-type hf \
+   --save-model-type mg \
    --target-tensor-parallel-size 8 \
    --target-pipeline-parallel-size 2 \
    --load-dir ./model_from_hf/Yi-hf/ \
    --save-dir ./model_weights/Yi-legacy/ \
    --tokenizer-model ./model_from_hf/Yi-hf/tokenizer.model \
-   --params-dtype bf16
+   --params-dtype bf16 \
+   --model-type-hf llama2
